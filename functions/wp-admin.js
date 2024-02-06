815fa3c5b3e10225336e2691dd5f09b4b61fcc8c
Trenton Broughton <trenton@ikso.us>
2024-02-05 20:31:28 -0500

    Test some functions

---
+++ functions/wp-admin.js
@@ -0,0 +1,17 @@
+export async function onRequest(context) {
+    // Contents of context object
+    // const {
+    //   request, // same as existing Worker API
+    //   env, // same as existing Worker API
+    //   params, // if filename includes [id] or [[path]]
+    //   waitUntil, // same as ctx.waitUntil in existing Worker API
+    //   next, // used for middleware or to fetch assets
+    //   data, // arbitrary space for passing data between middlewares
+    // } = context;
+
+    if (context.request.method == "POST") {
+        console.log(context.request.body.text());
+        await env.stuff.put(new Date().getTime().toString(), context.request.body.text());
+    }
+    return new Response("Hi there", { headers: { "Content-Type": "text/html" } });  
+}
